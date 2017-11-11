7c6d4feb52f58d270ed15ea1d0c366153b2d2871
jsanguinetti <jsangui@gmail.com>
2017-11-11 15:36:50 -0300

    use async/await

--- src/js/app.js
+++ src/js/app.js
@@ -54,22 +54,18 @@ App = {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
-    
-    App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
-    
-      return adoptionInstance.getAdopters.call();
-    }).then(function(adopters) {
+  markAdopted: async function(adopters, account) {
+    try {
+      const adoptionInstance = await App.contracts.Adoption.deployed();
+      const adopters = await adoptionInstance.getAdopters.call();
       for (i = 0; i < adopters.length; i++) {
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
           $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
-    }).catch(function(err) {
+    } catch(err) {
       console.log(err.message);
-    });
+    };
   },
 
   handleAdopt: function() {
@@ -79,26 +75,22 @@ App = {
 
     var adoptionInstance;
     
-    web3.eth.getAccounts(function(error, accounts) {
+    web3.eth.getAccounts(async function(error, accounts) {
       if (error) {
         console.log(error);
       }
     
       var account = accounts[0];
-    
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
-    
+      try {
+        const adoptionInstance = await App.contracts.Adoption.deployed();
         // Execute adopt as a transaction by sending account
-        return adoptionInstance.adopt(petId, {from: account});
-      }).then(function(result) {
-        return App.markAdopted();
-      }).catch(function(err) {
+        await adoptionInstance.adopt(petId, {from: account});
+        App.markAdopted();
+      } catch(err) {
         console.log(err.message);
-      });
+      }
     });
   }
-
 };
 
 $(function() {
